f398780cb20d3ccf3dacb99fb76ccd9dad7a39b3
Shayne3000 <alfred3000prime@hotmail.com>
2025-06-04 21:05:12 -0400

    leverage the builder to setup the Room Database

--- multiplatform/src/commonMain/kotlin/com/senijoshua/donezo/data/local/DonezoDatabase.kt
+++ multiplatform/src/commonMain/kotlin/com/senijoshua/donezo/data/local/DonezoDatabase.kt
@@ -4,10 +4,12 @@ import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.senijoshua.donezo.data.local.character.CharacterDao
 import com.senijoshua.donezo.data.local.character.CharacterEntity
 import com.senijoshua.donezo.data.local.task.TaskDao
 import com.senijoshua.donezo.data.local.task.TaskEntity
+import kotlinx.coroutines.CoroutineDispatcher
 
 @Database(entities = [TaskEntity::class, CharacterEntity::class], version = 1)
 @ConstructedBy(AppDatabaseConstructor::class)
@@ -23,4 +25,15 @@ abstract class DonezoDatabase : RoomDatabase() {
 @Suppress("NO_ACTUAL_FOR_EXPECT")
 expect object AppDatabaseConstructor : RoomDatabaseConstructor<DonezoDatabase> {
     override fun initialize(): DonezoDatabase
+}
+
+fun getRoomDatabase(
+    builder: RoomDatabase.Builder<DonezoDatabase>,
+    coroutineDispatcher: CoroutineDispatcher
+): DonezoDatabase {
+    return builder
+        .addMigrations()
+        .setDriver(BundledSQLiteDriver())
+        .setQueryCoroutineContext(coroutineDispatcher)
+        .build()
 }
