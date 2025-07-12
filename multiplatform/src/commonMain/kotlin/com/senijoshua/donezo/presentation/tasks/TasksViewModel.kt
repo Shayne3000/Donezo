0981666b215be4169eec7215df86f4cab05933a4
Shayne3000 <alfred3000prime@hotmail.com>
2025-07-11 22:37:25 -0400

    Setup the ui state and event flows

--- multiplatform/src/commonMain/kotlin/com/senijoshua/donezo/presentation/tasks/TasksViewModel.kt
+++ multiplatform/src/commonMain/kotlin/com/senijoshua/donezo/presentation/tasks/TasksViewModel.kt
@@ -1,14 +1,29 @@
 package com.senijoshua.donezo.presentation.tasks
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.flow
 
 class TasksViewModel : ViewModel() {
     // Setup state holder flow for ui state
 
     // Setup shared flow for one-time UI events
+    private val _uiEvent = MutableSharedFlow<TasksUIEvent>(
+        replay = 0,
+        extraBufferCapacity = 1 // to avoid dropping events when no collector is immediately ready.
+    )
+    val uiEvent: SharedFlow<TasksUIEvent> = _uiEvent
 
     // Setup cold flow to get Tasks from the DB
-
+    val uiState = flow {
+        try {
+            // TODO Call get tasks from repository
+            emit(TasksUIState.Success(tasks = emptyList()))
+        } catch (e: Exception) {
+            _uiEvent.emit(TasksUIEvent.Error(e.message))
+        }
+    }
     // Create, Update, Delete, and Mark Task as Completed operations
 }
 
@@ -24,5 +39,5 @@ sealed interface TasksUIState {
  * Denotation of the various UI events that occur in TasksScreen
  */
 sealed interface TasksUIEvent {
-    data class Error(val message: String) : TasksUIEvent
+    data class Error(val message: String?) : TasksUIEvent
 }
