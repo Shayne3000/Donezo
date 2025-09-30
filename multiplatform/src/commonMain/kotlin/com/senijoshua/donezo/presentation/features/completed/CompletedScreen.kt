5733c1fa776feb741137caceafb4b3517036cfce
Shayne3000 <alfred3000prime@hotmail.com>
2025-09-29 21:53:37 -0400

    setup completed screen structure

--- multiplatform/src/commonMain/kotlin/com/senijoshua/donezo/presentation/features/completed/CompletedScreen.kt
+++ multiplatform/src/commonMain/kotlin/com/senijoshua/donezo/presentation/features/completed/CompletedScreen.kt
@@ -1,8 +1,22 @@
 package com.senijoshua.donezo.presentation.features.completed
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.senijoshua.donezo.presentation.theme.DonezoTheme
+import com.senijoshua.donezo.presentation.utils.getGenericErrorMessage
+import kotlinx.coroutines.flow.SharedFlow
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -10,19 +24,48 @@ import org.koin.compose.viewmodel.koinViewModel
 fun CompletedScreen(
     viewModel: CompletedViewModel = koinViewModel()
 ) {
-    // Collect UiState as lifecycle
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val uiEvent = viewModel.uiEvent
 
-    // internal content composable
-
+    CompletedContent(
+        uiState = uiState,
+        uiEvent = uiEvent
+    )
 }
 
 @Composable
-private fun CompletedContent() {
-    // Setup snackbar
+private fun CompletedContent(
+    modifier: Modifier = Modifier,
+    uiState: CompletedUiState,
+    uiEvent: SharedFlow<CompletedUiEvent>
+) {
+    val snackBarHostState = remember { SnackbarHostState() }
+    val genericErrorMessage = getGenericErrorMessage()
 
-    // Setup event handling for any errors
+    Scaffold(snackbarHost = {
+        SnackbarHost(hostState = snackBarHostState) { data ->
+            Snackbar(
+                snackbarData = data,
+                containerColor = MaterialTheme.colorScheme.error,
+                contentColor = MaterialTheme.colorScheme.onError
+            )
+        }
+    }) { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
 
-    // Setup Scaffold
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        uiEvent.collect { events ->
+            when (events) {
+                is CompletedUiEvent.Error -> {
+                    val message = events.message ?: genericErrorMessage
+                    snackBarHostState.showSnackbar(message)
+                }
+            }
+        }
+    }
 }
 
 @Preview
--- multiplatform/src/commonMain/kotlin/com/senijoshua/donezo/presentation/features/completed/CompletedViewModel.kt
+++ multiplatform/src/commonMain/kotlin/com/senijoshua/donezo/presentation/features/completed/CompletedViewModel.kt
@@ -3,7 +3,7 @@ package com.senijoshua.donezo.presentation.features.completed
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.senijoshua.donezo.data.repository.TasksRepository
-import com.senijoshua.donezo.presentation.features.tasks.model.PresentationTask
+import com.senijoshua.donezo.presentation.model.PresentationTask
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -11,14 +11,15 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
-class CompletedViewModel(tasksRepository: TasksRepository) : ViewModel() {
+class CompletedViewModel(private val repository: TasksRepository) : ViewModel() {
     // Setup a shared-flow for one-time events like an error or navigation event
     private val _uiEvent = MutableSharedFlow<CompletedUiEvent>(replay = 0, extraBufferCapacity = 1)
     val uiEvent: SharedFlow<CompletedUiEvent> = _uiEvent
 
     // Setup an observable data holder
-    val uiState: StateFlow<CompletedUiState> = tasksRepository.getCompletedTasks()
+    val uiState: StateFlow<CompletedUiState> = repository.getCompletedTasks()
         .onEach { result ->
             if (result.isFailure) {
                 val error = result.exceptionOrNull()
@@ -33,26 +34,30 @@ class CompletedViewModel(tasksRepository: TasksRepository) : ViewModel() {
             initialValue = CompletedUiState.Loading
         )
 
-    // Retrieve the list of completed tasks from the Room DB
-    fun getCompletedTasks() {
-
+    fun markTaskAsTodo(id: Int) {
+        viewModelScope.launch {
+            repository.toggleTaskCompleteStatus(id)
+        }
     }
 
-    fun markTaskAsTodo(id: String) {
-
-    }
-
-    fun deleteCompletedTask(id: String) {
-
+    fun deleteCompletedTask(id: Int) {
+        viewModelScope.launch {
+            repository.deleteTask(id)
+        }
     }
 }
 
-// Setup representation of the UI state at each instant in time
+/**
+ * Representation of the UI state at any instant in time
+ */
 sealed interface CompletedUiState {
     data class Success(val completedTasks: List<PresentationTask>) : CompletedUiState
     data object Loading : CompletedUiState
 }
 
+/**
+ * Denotation of one-time ui events like error messages/navigation
+ */
 sealed interface CompletedUiEvent {
     data class Error(val message: String?) : CompletedUiEvent
 }
